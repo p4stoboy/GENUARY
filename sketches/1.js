e09ebd4cb8d34a97bb7a1ee3a99c456fa386a0ee
p4stoboy <b1gh@protonmail.com>
2021-01-02 15:40:52 +1100

    Fixing bizarre choices in 1.js

--- sketches/1.js
+++ sketches/1.js
@@ -5,30 +5,23 @@ let step = 0;
 
 function setup() {
     let myCanvas = createCanvas(400, 400);
-    myCanvas.parent("sketch");    const w = myCanvas.width;
-    init();
-}
-
-function init() {
-    const h = height;
-    const w = width;
+    myCanvas.parent("sketch");
     size = 40;
-    rows = w / size;
-    cols = h / size
-    console.log(rows);
-}
-
-function draw() {
+    rows = width / size;
+    cols = height / size;
+    rectMode(CENTER);
     stroke(0);
     strokeWeight(3);
     colorMode(HSB, 255);
+}
+
+function draw() {
     step++;
     for (let x = 0; x < width; x += size) {
         for (let y = 0; y < height; y += size) {
             for (let z = 1; z < 3; z++){
                 let c2 = color(((step + 255/z) / 2) % 255, 255, 255, 150);
                 fill(c2);
-                rectMode(CENTER);
                 rect(x + size / 2,
                     y + size / 2,
                     step % (size / z),
